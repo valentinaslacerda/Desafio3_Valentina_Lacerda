c80cb42f651d6eda6d50b52bb8d1cfb6b667bf42
Kaynã Mesquita <mesquitakayna@gmail.com>
2024-10-29 16:44:41 -0300

    chore: add files for all migrations

---
+++ src/infra/migrations/1730231007169-CreateUsers.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateUsers1730231007169 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
