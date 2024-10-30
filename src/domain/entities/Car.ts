6f3d2231d0b2da48702720584027bd19518cc0fe
Kaynã Mesquita <mesquitakayna@gmail.com>
2024-10-30 15:27:26 -0300

    feat: add car entity

---
+++ src/domain/entities/Car.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity('car')
+export class Car {
+  @PrimaryGeneratedColumn('uuid')
+  id: string | null;
+
+  @Column({ type: 'varchar', length: 7 })
+  plate: string | null;
+
+  @Column({ type: 'varchar', length: 127 })
+  brand: string | null;
+
+  @Column({ type: 'varchar', length: 127 })
+  model: string | null;
+
+  @Column({ type: 'int' })
+  km: number | null;
+
+  @Column({ type: 'int' })
+  year: number | null;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  price: number | null;
+
+  @Column({ type: 'varchar', length: 8 })
+  status: string | null;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date | null;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date | null;
+}
+
+export default Car;
