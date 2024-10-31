7f7e5d5d5fb04f5aebfb2895922e0443400f586f
Valentina Lacerda <valentinaslacerda07@gmail.com>
2024-10-31 14:49:31 -0300

    feat: add CreateClientService for client creation functionality

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+const ClientController = require('../http/controller/ClientController');
 const UserController = require('../http/controller/UserController');
 
 const routes = Router();
 
 routes.get('/user', UserController.index);
-
+routes.post('/client', ClientController.create);
 export default routes;
