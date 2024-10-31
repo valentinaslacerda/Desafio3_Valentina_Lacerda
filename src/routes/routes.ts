199344969949e18282f0edaa0806060e8dbb4814
cesaraugustomt <contatocsaugusto@gmail.com>
2024-10-31 14:26:20 -0300

    feat: updated user controller

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,5 +4,6 @@ const UserController = require('../http/controller/UserController');
 const routes = Router();
 
 routes.get('/user', UserController.index);
+routes.post('/user', UserController.create);
 
 export default routes;
