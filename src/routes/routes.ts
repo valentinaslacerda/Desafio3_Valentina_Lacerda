49872f8b709ec44c890e3ef4b0a144392b001526
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-11-01 12:15:56 -0300

    feat: update routes.ts to include Order routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+const OrderController = require('../http/controller/OrderController');
 const UserController = require('../http/controller/UserController');
 
 const routes = Router();
 
 routes.get('/user', UserController.index);
-routes.post('/user', UserController.create);
+routes.post('/orders', OrderController.create);
+routes.get('/orders/:id', OrderController.findById);
+routes.get('/orders', OrderController.list);
+routes.put('/orders/:id', OrderController.update);
+routes.delete('/orders/:id', OrderController.delete);
 
 export default routes;
