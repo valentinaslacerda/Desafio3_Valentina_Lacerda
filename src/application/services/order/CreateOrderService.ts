b59f69119ff70e93b9cf4b6e74902db51d9372f5
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-11-01 12:01:45 -0300

    feat: implement CreateOrderService to handle new order creation

---
+++ src/application/services/order/CreateOrderService.ts
@@ -0,0 +1,57 @@
+import { Order } from '../../../domain/entities/Order';
+import { CarRepository } from '../../../domain/repositories/CarRepository';
+import ClientsRepository from '../../../domain/repositories/ClientsRepository';
+import { OrderRepository } from '../../../domain/repositories/OrderRepository';
+import { CreateOrderDTO } from '../../../http/dtos/CreateOrder.dto';
+
+export class CreateOrderService {
+  private orderRepository: OrderRepository;
+  private clientRepository: ClientsRepository;
+  private carRepository: CarRepository;
+
+  constructor(
+    orderRepository: OrderRepository,
+    clientRepository: ClientsRepository,
+    carRepository: CarRepository
+  ) {
+    this.orderRepository = orderRepository;
+    this.clientRepository = clientRepository;
+    this.carRepository = carRepository;
+  }
+
+  public async execute({ clientId, carId }: CreateOrderDTO): Promise<Order> {
+    // check by id if a client exists
+    const client = await this.clientRepository.findById(clientId);
+    if (!client) {
+      throw new Error('Cliente não encontrado');
+    }
+
+    // check by id if a car exists
+    const car = await this.carRepository.findById(carId);
+    if (!car) {
+      throw new Error('Carro não encontrado');
+    }
+
+    // checks if a customer has an open order
+    const existingOrder =
+      await this.orderRepository.findOpenOrderByClientId(clientId);
+    if (existingOrder) {
+      throw new Error('O cliente já possui um pedido aberto');
+    }
+
+    const order = this.orderRepository.createOrder({
+      client,
+      car,
+      status: 'Aberto',
+      finalDate: null,
+      cancellationDate: null,
+      cep: null,
+      city: null,
+      state: null,
+      totalValue: 0,
+      createdAt: new Date(),
+    });
+
+    return order;
+  }
+}
