2ff42cc06adb2f945429bb85f3b95dbb23445b49
cesaraugustomt <contatocsaugusto@gmail.com>
2024-11-02 20:10:15 -0300

    feat: add seed main `MainSeeder`

---
+++ src/infra/seeds/MainSeeder.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import { runSeeder, Seeder, SeederFactoryManager } from 'typeorm-extension';
+import { UserSeeder } from './UserSeeder';
+
+export class MainSeeder implements Seeder {
+  track?: boolean | undefined;
+  async run(
+    dataSource: DataSource,
+    factoryManager: SeederFactoryManager
+  ): Promise<void> {
+    await runSeeder(dataSource, UserSeeder);
+  }
+}
