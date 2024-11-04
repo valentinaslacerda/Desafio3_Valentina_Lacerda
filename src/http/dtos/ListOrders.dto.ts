b751947325710b81e51f014289f216e32751e0eb
luizfernando05 <luiz.fernando.lf2016@outlook.com>
2024-11-04 12:18:17 -0300

    docs: update Readme

--- src/http/dtos/ListOrders.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface ListOrdersDTO {
-  status?: string;
-  clientCpf?: string;
-  startDate?: Date;
-  endDate?: Date;
-  sortOrder?: 'ASC' | 'DESC';
-  page?: number;
-  limit?: number;
-}
